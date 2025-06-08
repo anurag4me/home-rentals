6267f03b45ed2924f1d23bf79c5b79705c549f42
anurag4me <anuragmishra5033@gmail.com>
2025-06-09 00:19:39 +0530

    Setup register route in auth.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
-require('dotenv').config()
-const express = require('express');
-const mongoDbConnect = require('./connection');
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const mongoDbConnect = require("./connection");
+const cors = require("cors");
+const authRoutes = require("./routes/auth.js");
 
 const PORT = process.env.PORT || 3000;
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
 mongoDbConnect(process.env.MONGO_URL)
-.then(()=>console.log('MongoDb Connected Successfully!'))
-.catch((err)=>console.log('MongoDb error:', err))
+  .then(() => console.log("MongoDb Connected Successfully!"))
+  .catch((err) => console.log("MongoDb error:", err));
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+  res.send("Hello World!");
+});
 
-app.listen(PORT, ()=>console.log(`Sever Started Successfully at Port: ${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Sever Started Successfully at Port: ${PORT}`)
+);
