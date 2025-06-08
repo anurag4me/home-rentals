69b7a11331b5da681ed381ae9df22b72e161e09b
anurag4me <anuragmishra5033@gmail.com>
2025-06-08 23:33:39 +0530

    Basic server setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express');
+const mongoDbConnect = require('./connection');
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3000;
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+mongoDbConnect(process.env.MONGO_URL)
+.then(()=>console.log('MongoDb Connected Successfully!'))
+.catch((err)=>console.log('MongoDb error:', err))
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(PORT, ()=>console.log(`Sever Started Successfully at Port: ${PORT}`))
